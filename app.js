d2cb0a97c4bba103c9ad8624c699ebe1f869ecd4
pongdanny <pongdaniel17@gmail.com>
2021-04-22 18:50:31 -0400

    setup gitignore and mongoatlas

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+
+mongoose
+  .connect(db, { useNewURlParser: true })
+  .then(() => console.log("Connected to DB Successfully"))
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World"));
 
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://danny:X3zGC6d0q9vDLSqn@cluster0.h3mef.mongodb.net/Cluster0?retryWrites=true&w=majority",
+};
